8078d965e117e42098c57202b40eca9b04b80691
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-04-10 14:56:55 +0530

    Add files via upload

---
+++ util/Functions/gui.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import uuid
+import re
+from random import randint
+import streamlit as st
+
+def create_st_button(link_text, link_url, hover_color="#e78ac3", st_col=None):
+    button_uuid = str(uuid.uuid4()).replace("-", "")
+    button_id = re.sub("\d+", "", button_uuid)
+
+    button_css = f"""
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }}
+            #{button_id}:hover {{
+                border-color: {hover_color};
+                color: {hover_color};
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: {hover_color};
+                color: white;
+                }}
+        </style> """
+
+    html_str = f'<a href="{link_url}" target="_blank" id="{button_id}";>{link_text}</a><br></br>'
+
+    if st_col is None:
+        st.markdown(button_css + html_str, unsafe_allow_html=True)
+    else:
+        st_col.markdown(button_css + html_str, unsafe_allow_html=True)
