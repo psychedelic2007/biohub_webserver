a6ce8651d62bba72561916d579907cbc98b46808
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-04-13 13:57:29 +0530

    Update biohub.py

--- biohub.py
+++ biohub.py
@@ -7,7 +7,9 @@ from multiapp import MultiApp
 from util.pages.home_page import home_page
 from util.pages.Preprocessing import preprocessing
 from util.pages.entropy_try import entropy
-from util.pages.combine_feature import combine_feature
+from util.pages.Feature1 import feature1
+from util.pages.Feature2 import feature2
+from util.pages.Feature3 import feature3
 from util.pages.new_model2 import new_model2
 from util.pages.Testing import testing
 from util.pages.Tutorial import tutorial
@@ -19,25 +21,14 @@ from util.pages.phylo_try import phylo_try
 from util.pages.phylo_try1 import phylo_try1
 from util.pages.Theory_corner import theory
 
-# Your Google Analytics tracking code
-ga_code = '''
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4VKP7EFRK2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-4VKP7EFRK2');
-</script>
-'''
-
 app = MultiApp()
 
 app.add_app("Home Page", home_page)
 app.add_app("Preprocessing", preprocessing)
 app.add_app("Entropy Calculation", entropy)
-app.add_app("Feature Calculation", combine_feature)
+app.add_app("Feature 1 Calculation", feature1)
+app.add_app("Feature 2 Calculation", feature2)
+app.add_app("Feature 3 Calculation", feature3)
 app.add_app("Model Training", new_model2)
 app.add_app("Testing", testing)
 app.add_app("Tutorial", tutorial)
