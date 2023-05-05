15d78f303142c3bb71bafcf0d2c39b651cd8dbd4
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-05 14:53:29 +0530

    Update Theory_corner.py

--- util/pages/Theory_corner.py
+++ util/pages/Theory_corner.py
@@ -20,3 +20,11 @@ def theory():
     )
     
     st.title("Theory Corner")
+    st.markdown("Welcome to the Theory Corner, where all your burning questions about our web servers are answered! From the basics of server architecture to the "
+                "mathematical intricacies of the services that our webserver provides, this page is your one-stop-shop for all things theoretical in the world of "
+                "viral bioinformatics. Whether you're a seasoned bioinformatician or just starting out, our webserver and tutorials will help you provide "
+                "a platform that will allow you to seamlessly use the mathematical and machine learning techniques for biological exploration. So grab a cup of coffee, "
+                "settle in, and get ready to level up your knowledge with the Theory Corner!")
+    
+    st.write("")
+    st.write("***")
