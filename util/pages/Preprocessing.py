544af0e12f72752821afaaa477567dda4de37d3a
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-13 13:41:19 +0530

    Update Preprocessing.py

--- util/pages/Preprocessing.py
+++ util/pages/Preprocessing.py
@@ -75,7 +75,7 @@ def preprocessing():
 
                     #preprocess the sequences
                     processed_records = preprocess_sequences(records)
-                    sequence_filtered = filter_sequence(processed_records)
+                    sequence_filtered = filter_sequences(processed_records)
 
                     #download the preprocessed file
                     if(len(sequence_filtered)>0):
