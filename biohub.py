a422029d1aa093334171955f5dd303a3d0bdf4e5
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-17 10:40:55 +0530

    Update biohub.py

--- biohub.py
+++ biohub.py
@@ -11,14 +11,8 @@ from util.pages.Feature1 import feature1
 from util.pages.Feature2 import feature2
 from util.pages.Feature3 import feature3
 from util.pages.new_model2 import new_model2
-from util.pages.Testing import testing
 from util.pages.Tutorial import tutorial
 from util.pages.About import about
-from util.pages.final_ml_model_training import training
-from util.pages.entropy_calculation import mrf
-from util.pages.phylo import phylo
-from util.pages.phylo_try import phylo_try
-from util.pages.phylo_compare import phylo_compare
 from util.pages.Theory_corner import theory
 
 app = MultiApp()
@@ -30,13 +24,7 @@ app.add_app("Feature 1 Calculation", feature1)
 app.add_app("Feature 2 Calculation", feature2)
 app.add_app("Feature 3 Calculation", feature3)
 app.add_app("Model Training", new_model2)
-app.add_app("Testing", testing)
 app.add_app("Tutorial", tutorial)
 app.add_app("Theory Corner", theory)
 app.add_app("About", about)
-app.add_app("Training", training)
-app.add_app("MRF", mrf)
-app.add_app("Phylo", phylo)
-app.add_app("Phylo Try", phylo_try)
-app.add_app("Phylo compare", phylo_compare)
 app.run()
